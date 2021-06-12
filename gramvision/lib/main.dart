39315860ffd78f070f53a0beb6a3ced9148c1b19
Anurag Yadav <55681370+anuragyadav19150@users.noreply.github.com>
2021-06-13 00:17:07 +0530

    Add files via upload

---
+++ gramvision/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:gramvision/Homepage/HomeScreen.dart';
+import 'package:gramvision/Login/Login.dart';
+import 'package:gramvision/Login/SignUp.dart';
+import 'package:gramvision/Login/Start.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+      routes: <String, WidgetBuilder>{
+        "Login": (BuildContext context) => Login(),
+        "SignUp": (BuildContext context) => SignUp(),
+        "start": (BuildContext context) => Start(),
+      },
+    );
+  }
+}
